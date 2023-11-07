e878f2a095dbcdfd1cd4a84fde8b271c75be30c6
JimJam47 <allensreebinoj2010@gmail.com>
2023-11-07 15:34:40 +0530

    GREEN: Function to display grid
    Implemented the display function succesfully.
    Changed strings in earlier test cases into integers 0 and 1 for better implementation.

--- game_of_life.py
+++ game_of_life.py
@@ -1,10 +1,23 @@
 def get_grid(board):
     max_row,max_col = board['dimension']
-    grid = [['-']*max_col for _ in range(max_row)]
+    grid = [[0]*max_col for _ in range(max_row)]
     return grid
 
 def get_alive_cell(grid,board):
-    grid = get_grid(board)
+    
     alive_row,alive_col = board['alive']
-    grid[alive_row][alive_col] = '*'
+    grid[alive_row][alive_col] = 1
     return grid
+
+def display_grid(grid):
+    row = len(grid)
+    col =  len(grid[0])
+    display = ""
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                display += "* "
+            else:
+                display += "- "
+        display += "\n"
+    return display
