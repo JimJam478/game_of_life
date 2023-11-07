31315ab8aa8f3bff5773de28b74a2ea5143af9c5
JimJam47 <allensreebinoj2010@gmail.com>
2023-11-07 13:24:21 +0530

    GREEN: Function to make cell alive

--- game_of_life.py
+++ game_of_life.py
@@ -3,3 +3,8 @@ def get_grid(board):
     grid = [['-']*max_col for _ in range(max_row)]
     return grid
 
+def get_alive_cell(grid,board):
+    grid = get_grid(board)
+    alive_row,alive_col = board['alive']
+    grid[alive_row][alive_col] = '*'
+    return grid
