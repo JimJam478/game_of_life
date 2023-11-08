f41474e020551d8731bd2d079f7b0124676932c1
JimJam47 <allensreebinoj2010@gmail.com>
2023-11-08 10:53:41 +0530

    RED: Function to get the next generation

--- test_game_of_Life.py
+++ test_game_of_Life.py
@@ -36,5 +36,15 @@ def test_neighbour_count():
     count = game_of_life.get_neighbour_count(grid)
     assert count == neighbours
 
+def test_game_rules():
+    grid = [[0,0,0],
+            [1,1,1],
+            [0,0,0]]
+    neighbours = [[2,3,2],[1,2,1],[2,3,2]]
+    result = game_of_life.get_next_Gen(grid,neighbours)
+    assert result == [[0,1,0],
+                      [0,1,0],
+                      [0,1,0]]
+
 
     
