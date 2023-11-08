f3af5384e7cffebf27978f7d248ead8cc8a97c38
JimJam47 <allensreebinoj2010@gmail.com>
2023-11-08 20:55:55 +0530

    Fixes some errors in main function
    Removed code for addition of active cell to generated grid.
    (Implementation unsuccessful)
    
    Reformatted file using black
    
    Checked if all tests still pass.

--- game_of_life.py
+++ game_of_life.py
@@ -1,18 +1,19 @@
 def get_grid(board):
-    max_row,max_col = board['dimension']
-    grid = [[0]*max_col for _ in range(max_row)]
+    max_row, max_col = board["dimension"]
+    grid = [[0] * max_col for _ in range(max_row)]
     return grid
 
-def get_alive_cell(grid,board):
+
+def get_alive_cell(grid, board):
     alive_cells = board["alive"]
     for x, y in alive_cells:
         grid[x][y] = 1
-
     return grid
+
 
 def display_grid(grid):
     row = len(grid)
-    col =  len(grid[0])
+    col = len(grid[0])
     display = ""
     for i in range(row):
         for j in range(col):
@@ -22,6 +23,7 @@ def display_grid(grid):
                 display += "   |"
         display += "\n"
     return display
+
 
 def get_neighbour_count(grid):
     rows = len(grid)
@@ -33,73 +35,64 @@ def get_neighbour_count(grid):
             for x in [-1, 0, 1]:
                 for y in [-1, 0, 1]:
                     if x == 0 and y == 0:
-                        continue  
-                    new_i,new_j = i + x,j + y 
+                        continue
+                    new_i, new_j = i + x, j + y
                     if 0 <= new_i < rows and 0 <= new_j < cols:
                         if grid[new_i][new_j] == 1:
                             neighbors_count += 1
             neighbours[i][j] = neighbors_count
     return neighbours
 
-def get_next_Gen(grid,neighbours):
+
+def get_next_Gen(grid, neighbours):
     row = len(grid)
     col = len(grid[0])
     for i in range(row):
         for j in range(col):
             if grid[i][j]:
-                if neighbours[i][j] not in [2,3]:
+                if neighbours[i][j] not in [2, 3]:
                     grid[i][j] = 0
             if not grid[i][j]:
                 if neighbours[i][j] == 3:
                     grid[i][j] = 1
-    return grid 
+    return grid
+
 
 def main():
     board = {}
-    row = int(input('Enter dimension (row): '))
-    col = int(input('Enter dimension (col): '))
-    board["dimension"] = (row,col)
+    row = int(input("Enter dimension (row): "))
+    col = int(input("Enter dimension (col): "))
+    board["dimension"] = (row, col)
     grid = get_grid(board)
     print(display_grid(grid))
     board["alive"] = []
     while True:
-        starter = input('Add an alive cell ? (y/n): ')
-        if starter in ['Y','y']:
-            alive_row = int(input('Enter alive cell row: '))
-            alive_col = int(input('Enter alive cells col: '))
-            board["alive"].append((alive_row,alive_col))
-            alive_grid = get_alive_cell(grid,board)
+        starter = input("Add an alive cell ? (y/n): ")
+        if starter in ["Y", "y"]:
+            alive_row = input("Enter alive cell row: ")
+            alive_col = input("Enter alive cells col: ")
+            board["alive"].append((int(alive_row), int(alive_col)))
+            alive_grid = get_alive_cell(grid, board)
+            print(display_grid(alive_grid))
         else:
             break
-    print(display_grid(alive_grid))
+
     print("---------------------------------------------")
     while True:
-        cont = input("""See next generation. 
-If yes, enter c
-If no, enter s
-If you have to add another alive cell, enter a  
------------------> """)
-        if cont in ['c','C']:
+        cont = input(
+            """Generate the next generation: 
+Yes (c)
+No (s)
+> """
+        )
+        if cont in ["c", "C"]:
             neighbours = get_neighbour_count(alive_grid)
-            next_gen = get_next_Gen(alive_grid,neighbours)
+            next_gen = get_next_Gen(alive_grid, neighbours)
             print(display_grid(next_gen))
-        if cont in ['a','A']:
-            alive_row = int(input('Enter alive cell row: '))
-            alive_col = int(input('Enter alive cells col: '))
-            board["alive"].append((alive_row,alive_col))
-            new_grid = get_alive_cell(next_gen,board)
-            print(display_grid(new_grid))
-        if cont in ['s','S']:
+        if cont in ["s", "S"]:
             break
     print("Game stopped !!")
-        
+
+
 if __name__ == "__main__":
     main()
-
-
-    
-        
-
-
-
-
