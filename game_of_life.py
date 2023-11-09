ce22dd352f1d29c06aa9cc72b3d7b83a91f80ccb
JimJam47 <allensreebinoj2010@gmail.com>
2023-11-09 15:20:32 +0530

    Implements game of life automatically.

--- game_of_life.py
+++ game_of_life.py
@@ -57,55 +57,88 @@ def get_next_Gen(grid, neighbours):
                     grid[i][j] = 1
     return grid
 
-
+import random
 def main():
-    board = {}
-    row = int(input("Enter dimension (row): "))
-    col = int(input("Enter dimension (col): "))
-    board["dimension"] = (row, col)
-    grid = get_grid(board)
-    print(display_grid(grid))
-    board["alive"] = []
-    while True:
-        starter = input("Add an alive cell ? (y/n): ")
-        if starter in ["Y", "y"]:
-            alive_row = input("Enter alive cell row: ")
-            alive_col = input("Enter alive cells col: ")
-            board["alive"].append((int(alive_row), int(alive_col)))
-            alive_grid = get_alive_cell(grid, board)
-            print(display_grid(alive_grid))
-        else:
-            break
+    key = input("""AUTO or MANUAL:
+Auto (a)
+Manual (m)
+""")
+    if key == 'm':
+        board = {}
+        row = int(input("Enter dimension (row): "))
+        col = int(input("Enter dimension (col): "))
+        board["dimension"] = (row, col)
+        grid = get_grid(board)
+        print(display_grid(grid))
+        board["alive"] = []
+        while True:
+            starter = input("Add an alive cell ? (y/n): ")
+            if starter in ["Y", "y"]:
+                alive_row = input("Enter alive cell row: ")
+                alive_col = input("Enter alive cells col: ")
+                board["alive"].append((int(alive_row), int(alive_col)))
+                alive_grid = get_alive_cell(grid, board)
+                print(display_grid(alive_grid))
+            else:
+                break
 
-    print("---------------------------------------------")
-    while True:
-        cont = input(
-            """Generate the next generation: 
+        print("---------------------------------------------")
+        while True:
+            cont = input(
+                """Generate the next generation: 
 Yes (c)
 No (s)
 New Alive Cell (a)
 > """
-        )
-        if cont in ["c", "C"]:
-            neighbours = get_neighbour_count(alive_grid)
-            next_gen = get_next_Gen(alive_grid, neighbours)
-            print(display_grid(next_gen))
+            )
+            if cont in ["c", "C"]:
+                neighbours = get_neighbour_count(alive_grid)
+                next_gen = get_next_Gen(alive_grid, neighbours)
+                print(display_grid(next_gen))
+            
+            if cont in ['a','A']:
+                alive_row = input("Enter alive cell row: ")
+                alive_col = input("Enter alive cells col: ")
+                new_board = {}
+                new_board["alive"] = []
+                new_board["alive"].append((int(alive_row),int(alive_col)))
+                new_grid = get_alive_cell(next_gen, new_board)
+                print(display_grid(new_grid))
+                alive_grid = new_grid
+
+            if cont in ["s", "S"]:
+                break
+        print("Game stopped !!")
+    if key == 'a':
+        board = {}
+        row = random.randint(2,15)
+        col = row
+        board["dimension"] = row,col
+        grid = get_grid(board)
+        print(display_grid(grid))
+        board["alive"] = []
+        alive_cell_count = random.randint(0,row)
+        for i in range(alive_cell_count):
+            alive_row = random.randint(0,row)
+            alive_col = random.randint(0,col)
+            board["alive"].append((alive_row,alive_col))
+            alive_grid = get_alive_cell(grid, board)
+        print(display_grid(alive_grid))
         
-        if cont in ['a','A']:
-            alive_row = input("Enter alive cell row: ")
-            alive_col = input("Enter alive cells col: ")
-            new_board = {}
-            new_board["alive"] = []
-            new_board["alive"].append((int(alive_row),int(alive_col)))
-            new_grid = get_alive_cell(next_gen, new_board)
-            print(display_grid(new_grid))
-            alive_grid = new_grid
+        while True:
+            cont = input(
+                """Generate the next generation: 
+Enter < n >  to get next gen
+Enter < S > to stop 
+> """)
+            if cont == 'n':
+                neighbours = get_neighbour_count(alive_grid)
+                next_gen = get_next_Gen(alive_grid, neighbours)
+                print(display_grid(next_gen))
+            if cont in ['s','S']:
+                break
 
- 
-        if cont in ["s", "S"]:
-            break
-    print("Game stopped !!")
-
+    print("Game Stopped !!")
 
 if __name__ == "__main__":
     main()
